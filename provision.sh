f87b24d1cb0b2726ba299aec4a4c5d2f6c48b3d0
Hector Vido <hector_vido@yahoo.com.br>
2021-04-18 16:13:13 -0300

    Adicionado wicd e icone no systray

--- provision.sh
+++ provision.sh
@@ -3,9 +3,11 @@
 read -p 'Digite a senha do root: ' -s -r PASSWD
 
 SCRIPT='apt-get update
-apt-get install -y passwd wget vim xdm fluxbox git xinit firefox-esr thunar geany geany-plugin-treebrowser unzip vagrant sakura idesk
+apt-get dist-upgrade -y
+apt-get install -y passwd wget vim xdm fluxbox git xinit firefox-esr thunar geany geany-plugin-treebrowser unzip vagrant sakura idesk wicd
 echo deb https://download.virtualbox.org/virtualbox/debian buster contrib > /etc/apt/sources.list.d/virtualbox.list
 wget -O - https://www.virtualbox.org/download/oracle_vbox_2016.asc | apt-key add -
+wget -O - https://www.virtualbox.org/download/oracle_vbox.asc | apt-key add -
 apt-get update
 apt-get install -y virtualbox-6.0
 rm -rf /root/fluxbox-4linux
@@ -13,7 +15,7 @@ git clone https://github.com/hector-vido/fluxbox-4linux.git /root/fluxbox-4linux
 cp -a /root/fluxbox-4linux/xdm/* /etc/X11/xdm/
 for U in matutino vespertino noturno diurno sabado domingo; do
 	useradd -d /home/$U -m -s /bin/bash $U
-	usermod -G vboxusers -a -s /bin/bash $U
+	usermod -G vboxusers,netdev -a $U
 	echo -e "$U\n$U" | passwd $U
 	rm -rf /home/$U/.fluxbox
 	rm -rf /home/$U/.idesk*
@@ -24,7 +26,8 @@ for U in matutino vespertino noturno diurno sabado domingo; do
 	mkdir -p /home/$U/{Documentos,Downloads}
 	chown $U:$U /home/$U/{Documentos,Downloads}
 	chown -R $U:$U /home/$U/{.config,.mozilla} 
-done'
+done
+systemctl enable wicd'
 
 for X in $(seq 1 13); do 
 	sshpass -p "$PASSWD" ssh -o stricthostkeychecking=no suporte@192.168.203.$X "echo '$PASSWD' | su -c 'bash ${SCRIPT}' -" &
